247a2e9d44c52202c969ab52e9ee67eb7e43ffce
it21110016 <it21110016@my.sliit.lk>
2025-04-16 21:44:33 +0530

    update url with hosted one

--- frontend/src/utils/url.jsx
+++ frontend/src/utils/url.jsx
@@ -1,2 +1,2 @@
-export const auth_api =  "http://localhost:3000/api/v1/auth";
-export const user_api =  "http://localhost:5000/api/v1/user";
+export const auth_api =  "http://3.89.232.165:3000/api/v1/auth";
+export const user_api =  "http://3.89.232.165:5000/api/v1/user";
