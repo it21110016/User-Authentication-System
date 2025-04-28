494ecb2b5cb4c4b0b06fe7e7ca5b484154826d54
it21110016 <it21110016@my.sliit.lk>
2025-04-28 21:19:26 +0530

    update deployed url

--- frontend/src/utils/url.jsx
+++ frontend/src/utils/url.jsx
@@ -1,2 +1,2 @@
-export const auth_api =  "http://3.89.232.165:3000/api/v1/auth";
-export const user_api =  "http://3.89.232.165:5000/api/v1/user";
+export const auth_api =  "http://3.81.218.156:3000/api/v1/auth";
+export const user_api =  "http://3.81.218.156:5000/api/v1/user";
