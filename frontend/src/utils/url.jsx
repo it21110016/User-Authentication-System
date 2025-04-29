835579cbf7edd8a72252eda3c9d2e56256691756
it21110016 <it21110016@my.sliit.lk>
2025-04-29 10:44:05 +0530

    add aws api gateway

--- frontend/src/utils/url.jsx
+++ frontend/src/utils/url.jsx
@@ -1,2 +1,2 @@
-export const auth_api =  "http://18.234.172.162:3000/api/v1/auth";
-export const user_api =  "http://18.234.172.162:5000/api/v1/user";
+export const auth_api =  "https://3hbthzom88.execute-api.us-east-1.amazonaws.com:3000/api/v1/auth";
+export const user_api =  "https://3hbthzom88.execute-api.us-east-1.amazonaws.com:5000/api/v1/user";
